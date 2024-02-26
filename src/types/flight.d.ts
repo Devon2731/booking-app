25c0a0baddafab071058f3f433be48e3c9af91cc
Devon2731 <devon.whitaker27@gmail.com>
2024-02-25 20:42:52 -0500

    added types for folder with booking, flight, hotels, trips, and user typescript js

---
+++ src/types/flight.d.ts
@@ -0,0 +1,12 @@
+export interface FlightType {
+    id: number;
+    name: string;
+    logo: string;
+    from: string;
+    to: string;
+    departureTime: string;
+    arrivalTime: string;
+    price: number;
+    duration: string;
+    jobId: number;
+}
